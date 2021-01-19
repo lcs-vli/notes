4b5856deb57b0b50471f9c4f0ebe89c2e7ba3e58
Victoria Li <VLi@lcs.on.ca>
2021-01-19 09:41:35 -0500

    vale types using structures

---
+++ structureAndClasses.playground/Contents.swift
@@ -0,0 +1,30 @@
+import Cocoa
+
+struct Location {
+    var x: Int
+    var y: Int
+}
+
+//September at LCS
+var home = Location(x: 7, y: 10)
+var school = home
+
+//Where is home?
+home.x
+home.y
+
+//Where is school
+school.x
+school.y
+
+//Christmas vacation time
+home.x =  19
+home.y = 10
+
+//WHere is home?
+home.x
+home.y
+
+//Where is school
+school.x
+school.y
